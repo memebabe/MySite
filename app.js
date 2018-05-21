23d6db1d89f76081a5a1e18164f9b74d37a3d2de
memebabe <thanhvanqb3@gmail.com>
2018-05-21 07:10:17 +0700

    namhh

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/home');
-var usersRouter = require('./routes/users');
-var fibonacci = require('./routes/fibonacci');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -20,9 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/fibonacci', fibonacci);
+app.use('/*', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+    res.render('home', { images: ['images/a.jpg', 'images/a.jpg'] });
+  });
+
 module.exports = router;
