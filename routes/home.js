96e406161fc8f11abaae983f7615185671c9236a
memebabe <thanhvanqb3@gamil.com>
2018-05-24 17:02:24 +0700

    update

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('home', 
         {
-          posts: [
+          newPosts: [
             {
               image: "images/a.jpg",
               title: "post1",
