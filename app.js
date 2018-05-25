20a7d9de9ed43f283cae2f141b89396f8dd85640
memebabe <thanhvanqb3@gmail.com>
2018-05-25 09:39:14 +0700

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var homeRouter = require('./routes/home');
+var readRouter = require('./routes/read');
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/read', readRouter);
 app.use('/*', homeRouter);
 
 // catch 404 and forward to error handler
